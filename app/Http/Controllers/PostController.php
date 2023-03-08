d0c0b1be8ad4a2b0f1f32aee324969647ea0ef86
fihryal <fihryalfaqiy@gmail.com>
2023-03-08 14:50:47 +0700

    show id user (writer) as writer

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,6 +18,11 @@ class PostController extends Controller
     }
 
     public function show($id){
+        $post = Post::with('writer:id,username')->findOrFail($id);
+        return new DetailPostResourc($post);
+    }
+    
+    public function show2($id){
         $post = Post::findOrFail($id);
         return new DetailPostResourc($post);
     }
--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,4 @@ use Illuminate\Support\Facades\Route;
 
 route::get('/posts', [PostController::class ,'index']);
 route::get('/posts/{id}', [PostController::class ,'show']);
+route::get('/posts2/{id}', [PostController::class ,'show2']);
