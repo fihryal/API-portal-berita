5cacac27a902991b7ca03babbd96ad3e61b2e911
fihryal <fihryalfaqiy@gmail.com>
2023-03-14 14:13:22 +0700

    create access  token when login

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
 use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Laravel\Sanctum\Sanctum;
 
-route::get('/posts', [PostController::class ,'index']);
-route::get('/posts/{id}', [PostController::class ,'show']);
+route::get('/posts', [PostController::class ,'index'])->middleware(['auth:sanctum']);
+route::get('/posts/{id}', [PostController::class ,'show'])->middleware(['auth:sanctum']);
 route::get('/posts2/{id}', [PostController::class ,'show2']);
+
+route::post('/login',[AuthenticationController::class, 'login']);
