087d1ad050700db4527b1c93020a4352c248ca6e
fihryal <fihryalfaqiy@gmail.com>
2023-03-16 14:14:23 +0700

    create comment on some post

---
+++ app/Http/Controllers/commentController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Comment;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class commentController extends Controller
+{
+    public function store(Request $request)
+    {
+        $request -> validate([
+            'post_id' => 'required|exists:posts,id',
+            'comments_content' => 'required'
+        ]);
+
+        $request ['user_id'] = Auth()->user()->id;
+
+        $comment = Comment::create($request->all());
+
+        return response()->json($comment);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\commentController;
 use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,12 +11,14 @@ route::middleware(['auth:sanctum'])->group(function(){
     route::get('/posts', [PostController::class ,'index']);
     route::get('/posts/{id}', [PostController::class ,'show']);
     route::post('/posts',[PostController::class , 'store']);
+    route::get('/posts2/{id}', [PostController::class ,'show2']);
     route::patch('/posts/{id}',[PostController::class, 'update'])->middleware('post.owner');/*daftar mddleware di carnel.php*/
     route::delete('/posts/{id}',[PostController::class, 'delete'])->middleware('post.owner');
+
+    route::post('/comment',[commentController::class, 'store']);
     
     route::get('/logout',[AuthenticationController::class, 'logout']);
     route::get('/me',[AuthenticationController::class, 'me']);
 });
 
-route::get('/posts2/{id}', [PostController::class ,'show2']);
 route::post('/login',[AuthenticationController::class, 'login']);
